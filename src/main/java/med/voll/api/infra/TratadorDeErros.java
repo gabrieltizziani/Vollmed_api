a9224e8a61cab3990da68b2e5615c86b7ca90e47
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-09-25 08:39:16 -0300

    A

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -3,6 +3,7 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,6 +19,12 @@ public class TratadorDeErros {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
         var erros = exception.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros);
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+        public DadosErroValidacao (FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
