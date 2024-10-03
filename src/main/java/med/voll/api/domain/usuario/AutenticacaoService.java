ccb34082ff55de562738ae91ae1a7b6a5aed1942
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-10-03 19:12:43 -0300

    Git pull

---
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
