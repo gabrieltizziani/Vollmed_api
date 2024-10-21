61fb24cd2344178c8331d43ae10f5e2c66e5ee94
Gabriel de Melo Tizziani <126525209+gabrieltizziani@users.noreply.github.com>
2024-10-21 11:24:21 -0300

    Security

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -2,7 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
-import med.voll.api.domain.usuario.DadosTokenJWT;
+import med.voll.api.infra.security.DadosTokenJWT;
 import med.voll.api.domain.usuario.Usuario;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.Authenticator;
 
 @RestController
 @RequestMapping("/login")
